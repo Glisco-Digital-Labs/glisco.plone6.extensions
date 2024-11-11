4b07e6e26b09f08fab409ab1197cd6b60392f792
Joe Medicis <joe.medicis@gmail.com>
2024-11-11 10:49:43 +0000

    Still trying to overcome runtime error

--- src/glisco/plone6/extensions/vocabularies/utilities.py
+++ src/glisco/plone6/extensions/vocabularies/utilities.py
@@ -4,6 +4,16 @@ from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 
 @provider(IVocabularyFactory)
+def PageTypesVocabulary(context):
+    name = "glisco.vocabularies.pages"
+    registry_record_value = api.portal.get_registry_record(name)
+    items = registry_record_value.get("items", [])
+    lang = api.portal.get_current_language()
+    return SimpleVocabulary.fromItems(
+        [[item["token"], item["token"], item["titles"][lang]] for item in items]
+    )
+
+@provider(IVocabularyFactory)
 def MarketSegmentsVocabulary(context):
     name = "glisco.vocabularies.market_segments"
     registry_record_value = api.portal.get_registry_record(name)
