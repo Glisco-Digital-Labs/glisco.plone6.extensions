d43d5c6122c6777db52a6979e83fccc0b0ded106
Joe Medicis <joe.medicis@gmail.com>
2025-03-26 21:21:29 +0000

    generating full css bundle with exception handling

--- src/glisco/plone6/extensions/views/css_view.py
+++ src/glisco/plone6/extensions/views/css_view.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import pprint
 from Products.Five.browser import BrowserView
 from zope.interface import implementer
 from zope.interface import Interface
@@ -14,15 +13,35 @@ class ICssView(Interface):
 class CssView(BrowserView):
     
     def bundle(self):
-        
-        reg_record = api.portal.get_registry_record("glisco.extensions.settings.design.variables")
-        pprint.pprint("********************** CSS SETTINGS **********************")
-        pprint.pprint(reg_record)
-        return reg_record
+        prefix = "glisco.extensions.settings.design"
+        fields = ["variables", "base", "components", "animations", "utilities"]
+        bundle = """
+        /* ************************************************************************
+          bundle.css
+          --------
+          Site's bundled css.
+
+          Includes "variables", "base", "components", "animations", "utilities"
+
+        ************************************************************************ */
+
+        """
+        for field in fields:
+            try:
+                reg_record = api.portal.get_registry_record(f"{prefix}.{field}")
+                bundle += reg_record
+                bundle += "\n\n"
+            except:
+              bundle += """
+              /* ************************************************************************
+                {field}.css ERROR
+                ---------------
+                Could not load {field}.css
+              ************************************************************************ */
+
+              """.format(field=field)
+              bundle += "\n\n"
+        return bundle
     
     def __call__(self):
-        template = """<li class="heading" i18n:translate="">
-          Sample View
-        </li>"""
-        # return template
         return self.bundle()
