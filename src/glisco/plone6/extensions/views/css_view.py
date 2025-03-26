8ad60bc9ec0c4ab68a394a18d67e089ac9760927
Joe Medicis <joe.medicis@gmail.com>
2025-03-26 17:47:47 +0000

    Added site-css-bundle view using plonecli

---
+++ src/glisco/plone6/extensions/views/css_view.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import pprint
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.interface import Interface
+from plone import api
+from glisco.plone6.extensions.vocabularies.interfaces import IDesignSettings
+
+class ICssView(Interface):
+    """Marker Interface for ICssView"""
+
+@implementer(ICssView)
+class CssView(BrowserView):
+    
+    def bundle(self):
+        
+        reg_record = api.portal.get_registry_record('glisco.extensions.settings.design', interface=IDesignSettings)
+        pprint("********************** CSS SETTINGS **********************")
+        pprint(reg_record)
+        return reg_record
+    
+    def __call__(self):
+        template = """<li class="heading" i18n:translate="">
+          Sample View
+        </li>"""
+        # return template
+        return self.bundle()
