80875d1c6156dfa328f028f911370571d44525ee
Joe Medicis <joe.medicis@gmail.com>
2025-04-11 17:10:35 +0100

    Trying with static vocabularies

--- src/glisco/plone6/extensions/vocabularies/utilities.py
+++ src/glisco/plone6/extensions/vocabularies/utilities.py
@@ -1,7 +1,10 @@
 from plone import api
 from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import (
+    SimpleVocabulary,
+    SimpleTerm
+)
 
 from glisco.plone6.extensions.vocabularies.constants import (
     MARKET_SEGMENTS_TAXONOMY,
@@ -20,7 +23,20 @@ from glisco.plone6.extensions.vocabularies.constants import (
     TYPE_OF_PAGE_TAXONOMY
 )
 
-def loadVocabulary(name):
+def loadVocabularyFromStaticData(data):
+    lang = api.portal.get_current_language()
+    return SimpleVocabulary.fromItems(
+        [
+            [
+                item["token"],
+                item["token"],
+                item["titles"][lang],
+            ]
+            for item in data
+        ]
+    )
+
+def loadVocabularyFromPloneRegistry(name):
     registry_record_value = api.portal.get_registry_record(name)
     items = registry_record_value.get("items", [])
     lang = api.portal.get_current_language()
@@ -31,65 +47,65 @@ def loadVocabulary(name):
 
 @provider(IVocabularyFactory)
 def PageTypesVocabulary(context):
-    return loadVocabulary(TYPE_OF_PAGE_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(TYPE_OF_PAGE_TAXONOMY)
 
 
 @provider(IVocabularyFactory)
 def BlockLayoutTypesVocabulary(context):
-    return loadVocabulary(PAGE_BLOCK_LAYOUTS_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(PAGE_BLOCK_LAYOUTS_TAXONOMY)
 
 
 @provider(IVocabularyFactory)
 def MarketSegmentsVocabulary(context):
-    return loadVocabulary(MARKET_SEGMENTS_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(MARKET_SEGMENTS_TAXONOMY)
 
 
 @provider(IVocabularyFactory)
 def ProductSegmentsVocabulary(context):
-    return loadVocabulary(PRODUCT_SEGMENTS_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(PRODUCT_SEGMENTS_TAXONOMY)
 
 
 @provider(IVocabularyFactory)
 def ProductTypesVocabulary(context):
-    return loadVocabulary(PRODUCT_TYPES_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(PRODUCT_TYPES_TAXONOMY)
 
 
 @provider(IVocabularyFactory)
 def ProductMaterialsVocabulary(context):
-    return loadVocabulary(PRODUCT_MATERIALS_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(PRODUCT_MATERIALS_TAXONOMY)
 
 
 @provider(IVocabularyFactory)
 def ProductionTechniquesVocabulary(context):
-    return loadVocabulary(PRODUCTION_TECHNIQUES_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(PRODUCTION_TECHNIQUES_TAXONOMY)
 
 @provider(IVocabularyFactory)
 def PhotographyTonesVocabulary(context):
-    return loadVocabulary(PHOTOGRAPHY_TONES_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(PHOTOGRAPHY_TONES_TAXONOMY)
 
 @provider(IVocabularyFactory)
 def ImageTreatmentVocabulary(context):
-    return loadVocabulary(IMAGE_TREATMENT_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(IMAGE_TREATMENT_TAXONOMY)
 
 @provider(IVocabularyFactory)
 def ImageMoodDescriptorsVocabulary(context):
-    return loadVocabulary(IMAGE_MOOD_DESCRIPTORS_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(IMAGE_MOOD_DESCRIPTORS_TAXONOMY)
 
 @provider(IVocabularyFactory)
 def IllustrationTypesVocabulary(context):
-    return loadVocabulary(ILLUSTRATION_TYPES_TAXONOMY)
+    return loadVocabularyFromPloneRegistry(ILLUSTRATION_TYPES_TAXONOMY)
 
 @provider(IVocabularyFactory)
 def ThemesVocabulary(context):
-    return loadVocabulary(THEMES_TAXONOMY)
+    return loadVocabularyFromStaticData(THEMES_TAXONOMY)
 
 @provider(IVocabularyFactory)
 def SiteArchetypesVocabulary(context):
-    return loadVocabulary(SITE_ARCHETYPES_TAXONOMY)
+    return loadVocabularyFromStaticData(SITE_ARCHETYPES_TAXONOMY)
 
 @provider(IVocabularyFactory)
 def SiteTestVocabulary(context):
     print("**** >>>>> Loading test vocabulary. Got:")
-    data = loadVocabulary(TEST_TAXONOMY)
+    data = loadVocabularyFromStaticData(TEST_TAXONOMY)
     print(data)
     return data
