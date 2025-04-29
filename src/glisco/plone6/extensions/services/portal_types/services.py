2403cd07644c03b67d37a885bca87e701083f677
Joe Medicis <joe.medicis@gmail.com>
2025-04-29 15:12:11 +0100

    Trying to fix PortalTypesServiceGet

--- src/glisco/plone6/extensions/services/portal_types/services.py
+++ src/glisco/plone6/extensions/services/portal_types/services.py
@@ -1,16 +1,23 @@
+# This view is giving me a bit of an issue:
+# It is declared as belowL
+# <plone:service
+#         name="portal-types"
+#         method="GET"
+#         for="*"
+#         factory=".portal_types.services.PortalTypesServiceGet"
+#         permission="zope2.View" 
+#         /> 
+# But when it returns the data, it is not in JSON format.
+# It is returning the data in a non-json format, with ' instead of " in the data field of the result
+
 import json
 from plone import api
-from plone.rest.interfaces import IAPIRequest
 from plone.restapi.services import Service
 from Products.CMFCore.utils import getToolByName
 from zope.interface import providedBy
 
 class PortalTypesServiceGet(Service):
 
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request       
-        
     def reply(self):
         fields = []
         status = 200
@@ -30,6 +37,8 @@ class PortalTypesServiceGet(Service):
             print(cms_types)
             print("**************  fields (return)  **************")
             print(fields)
+            # This comes out as 
+            # ['Collection', 'Discussion Item', 'Document', 'Event', 'File', 'Folder', 'Image', 'LIF', 'LRF', 'Link', 'News Item', 'Plone Site', 'TempFolder']
             print("************** /EnabledPortalTypes **************")
 
         except Exception as e:
@@ -40,5 +49,7 @@ class PortalTypesServiceGet(Service):
         return {
             "status": status,
             "message": message,
-            "data": fields
+            "data": fields 
+            # "data" is the list of enabled portal types
+            # But is being rendered in non-json format, with ' instead of ". 
         }
